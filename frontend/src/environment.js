fa133e1ea120d4df07ba99c7eeb08f5f878894a1
Om942 <ommulmule14@gmail.com>
2025-01-13 16:24:56 +0530

    first commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -2,7 +2,7 @@
 
 let IS_PROD = true;
 const server = IS_PROD ? 
-    "https://netromybackend.onrender.com" : 
+    "https://netromybackend-ik0m.onrender.com" : 
     "http://localhost:8000";
 
 export default server;
